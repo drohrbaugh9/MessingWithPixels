f2afdd5b0705df3bb046ae64984112eaebfec2b2
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2016-11-03 12:07:39 -0600

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,37 @@
+public class Test {
+  public static void main(String[] args) {
+    boolean[] start = {true, true, true, false, false};
+    boolean[] end = {false, false, true, true, true};
+    boolean[] change = new boolean[start.length];
+    boolean[] middle = start;
+    
+    for (int i = 0; i < start.length; i++) {
+      change[i] = !(start[i] && end[i]);
+      System.out.println(change[i]);
+    }
+    
+    while(!equal(middle, end)) {
+      for (int i = 0; i < middle.length; i++) {
+        if (i == 0 && change[i] && middle[i] != middle[i + 1]) switch(middle, i);
+        else if (i == (middle.length - 1) && change
+      }
+    }
+  }
+  
+  public boolean equal(boolean[] a, boolean[] b) {
+    if (a.length != b.length) return false;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) return false;
+    }
+    return true;
+  }
+  
+  public void switch(boolean[] a, index i) {
+    if (a[i]) {
+      a[i] = false;
+      return;
+    } else if (!a[i]) {
+      a[i] = true;
+    }
+  }
+}
