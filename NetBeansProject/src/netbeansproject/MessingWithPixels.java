a1b197c778e6291b881a5c704958ae7690a26edb
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-11-04 12:00:23 -0600

    I think it works now

--- NetBeansProject/src/netbeansproject/MessingWithPixels.java
+++ NetBeansProject/src/netbeansproject/MessingWithPixels.java
@@ -7,44 +7,40 @@ public class MessingWithPixels {
 
     static ArrayList<Boolean> start; static boolean[] s = {false, true, true, true, false, false, false};
     static ArrayList<Boolean> end; static boolean[] e = {false, false, false, true, true, true, false};
-    static boolean[] change;
+    static ArrayList<Boolean> change;
     static ArrayList<Boolean> middle;
     static ArrayList<Boolean> newMiddle;
 
     public static void main(String[] args) {
-        change = new boolean[start.size()];
         ArrayListInit();
-
-        for (int i = 0; i < start.size(); i++) {
-            change[i] = !(start.get(i) == end.get(i));
-        }
-        System.out.println("change array: " + Arrays.toString(change));
+      	
+        //System.out.println("change array: " + change.toString());
         System.out.println("step 0: " + middle.toString());
 
         int j = 0;
-        while (!middle.equals(end)) /*j < 3*/ {
-            
-        }
-        
-        while (!middle.equals(end)) {
-            System.out.println("newmiddle: " + newMiddle.toString() + ", i: 0");
+        while (!middle.equals(end) && j < 6) {
+            //System.out.println("newmiddle: " + newMiddle.toString() + ", i: 0");
             for (int i = 1; i < (middle.size() - 1); i++) {
-                if (change[i] && (middle.get(i) != middle.get(i - 1) || middle.get(i) != middle.get(i + 1))) {
+                if (change.get(i) && (middle.get(i) != middle.get(i - 1) || middle.get(i) != middle.get(i + 1))) {
                     switcheroo(newMiddle, i);
                 }
-                System.out.println("newmiddle: " + newMiddle.toString() + ", i: " + i);
-                System.out.println("middle: " + newMiddle.toString());
+                //System.out.println("newmiddle: " + newMiddle.toString() + ", i: " + i);
+                //System.out.println("middle: " + newMiddle.toString());
             }
-            middle = newMiddle;
+            middle = new ArrayList<>(newMiddle);
             System.out.println("step " + (j + 1) + ": " + middle.toString());
+          	updateChange();
+          	j++;
         }
     }
 
     public static void switcheroo(ArrayList<Boolean> a, int i) {
         if (a.get(i)) {
+          	a.remove(i);
             a.add(i, false);
             return;
         } if (!a.get(i)) {
+          	a.remove(i);
             a.add(i, true);
         }
     }
@@ -55,12 +51,20 @@ public class MessingWithPixels {
             start.add(s[i]);
         }
         
-        end = new ArrayList<Boolean>();
+        end = new ArrayList<>();
         for (int i = 0; i < e.length; i++) {
             end.add(e[i]);
         }
-        
-        middle = start;
-        newMiddle = middle;
+      
+        middle = new ArrayList<>(start);
+        newMiddle = new ArrayList<>(middle);
+      	updateChange();
     }
+  	
+  	public static void updateChange() {
+    	change = new ArrayList<>();
+        for (int i = 0; i < start.size(); i++) {
+            change.add(!(middle.get(i) == end.get(i)));
+        }
+  	}
 }
