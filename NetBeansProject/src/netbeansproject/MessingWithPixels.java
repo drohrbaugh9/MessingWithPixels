b13ecd44a5044a0dd228e282aee24f344a5699ff
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-11-04 15:15:52 -0600

    Start transition to 2D and matrices

--- NetBeansProject/src/netbeansproject/MessingWithPixels.java
+++ NetBeansProject/src/netbeansproject/MessingWithPixels.java
@@ -1,70 +1,66 @@
-//package netbeansproject;
+package netbeansproject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MessingWithPixels {
 
-    static ArrayList<Boolean> start; static boolean[] s = {false, true, true, true, false, false, false};
-    static ArrayList<Boolean> end; static boolean[] e = {false, false, false, true, true, true, false};
-    static ArrayList<Boolean> change;
-    static ArrayList<Boolean> middle;
-    static ArrayList<Boolean> newMiddle;
+    static boolean M = true, _ = false;
+
+    static boolean[][] start = {
+        {_, _, _, _, _, _, _},
+        {_, M, M, M, M, _, _},
+        {_, M, M, M, M, _, _},
+        {_, _, _, _, _, _, _},
+        {_, _, _, _, _, _, _}};
+    static boolean[][] end = {
+        {_, _, _, _, _, _, _},
+        {_, _, _, _, _, _, _},
+        {_, _, M, M, M, M, _},
+        {_, _, M, M, M, M, _},
+        {_, _, _, _, _, _, _}};
+    static boolean[][] change;
+    static boolean[][] middle = start;
+    static boolean[][] newMiddle = middle;
 
     public static void main(String[] args) {
-        ArrayListInit();
-      	
+
         //System.out.println("change array: " + change.toString());
         System.out.println("step 0: [" + middle.toString().substring(8, middle.toString().length() - 8) + "]");
 
-        int j = 0;
-        while (!middle.equals(end) && j < 6) {
+        while (middle != end) {
             //System.out.println("newmiddle: " + newMiddle.toString() + ", i: 0");
-            for (int i = 1; i < (middle.size() - 1); i++) {
-                if (change.get(i) && (middle.get(i) != middle.get(i - 1) || middle.get(i) != middle.get(i + 1))) {
-                    switcheroo(newMiddle, i);
+            for (int i = 1; i < (middle.length - 1); i++) {
+                for (int j = 1; j < (middle[i].length - 1); j++) {
+                    if (change[i][j] && (middle.get(i) != middle.get(i - 1) || middle.get(i) != middle.get(i + 1))) {
+                        switcheroo(newMiddle, i);
+                    }
                 }
                 //System.out.println("newmiddle: " + newMiddle.toString() + ", i: " + i);
                 //System.out.println("middle: " + newMiddle.toString());
             }
             middle = new ArrayList<>(newMiddle);
             System.out.println("step " + (j + 1) + ": [" + middle.toString().substring(8, middle.toString().length() - 8) + "]");
-          	updateChange();
-          	j++;
+            updateChange();
+            j++;
         }
     }
 
-    public static void switcheroo(ArrayList<Boolean> a, int i) {
-        if (a.get(i)) {
-          	a.remove(i);
-            a.add(i, false);
+    public static void switcheroo(boolean[][] a, int i, int j) {
+        if (a[i][j]) {
+            a[i][j] = false;
             return;
-        } if (!a.get(i)) {
-          	a.remove(i);
-            a.add(i, true);
+        }
+        if (!a[i][j]) {
+            a[i][j] = true;
         }
     }
 
-    public static void ArrayListInit() {
-        start = new ArrayList<>();
-        for (int i = 0; i < s.length; i++) {
-            start.add(s[i]);
+    public static void updateChange() {
+        change = new boolean[start.length][start.length];
+        for (int i = 0; i < start.length; i++) {
+            for (int j = 0; j < start.length; j++) {
+                change[i][j] = (!(middle[i][j] == end[i][j]));
+            }
         }
-        
-        end = new ArrayList<>();
-        for (int i = 0; i < e.length; i++) {
-            end.add(e[i]);
-        }
-      
-        middle = new ArrayList<>(start);
-        newMiddle = new ArrayList<>(middle);
-      	updateChange();
     }
-  	
-  	public static void updateChange() {
-    	change = new ArrayList<>();
-        for (int i = 0; i < start.size(); i++) {
-            change.add(!(middle.get(i) == end.get(i)));
-        }
-  	}
 }
