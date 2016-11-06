b811f1e99ca24363f6349d0d19a399a77b24512d
Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-11-05 22:51:26 -0600

    2D basically working, edges are still a little weird

--- NetBeansProject/src/netbeansproject/MessingWithPixels.java
+++ NetBeansProject/src/netbeansproject/MessingWithPixels.java
@@ -1,38 +1,83 @@
 package netbeansproject;
 
-import java.util.Arrays;
-
 public class MessingWithPixels {
 
-    static final boolean M = true, L = false;
-
-    static final boolean[][] start = {
-        {L, L, L, L, L},
-        {L, M, L, L, L},
-        {L, M, L, L, L},
-        {L, M, L, L, L},
-        {L, M, L, L, L},
-        {L, M, M, M, L},
-        {L, L, L, L, L}};
-    static final boolean[][] end = {
-        {L, L, L, L, L},
-        {L, M, M, L, L},
-        {L, M, L, M, L},
-        {L, M, M, L, L},
-        {L, M, L, M, L},
-        {L, M, M, L, L},
-        {L, L, L, L, L}};
-    /*static final boolean[][] start = {
-        {L, L, L, L, L, L, L},
-        {L, M, M, M, L, L, L},
-        {L, L, L, L, L, L, L}};
-    static final boolean[][] end = {
-        {L, L, L, L, L, L, L},
-        {L, L, L, M, M, M, L},
-        {L, L, L, L, L, L, L}};*/
-    static boolean[][] change;
-    static boolean[][] middle;
-    static boolean[][] newMiddle;
+    static final int h = 2, l = 0, f = 1;
+    
+    static final int[][] A = {
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f},
+        {f,l,l,l,l,h,h,h,h,h,h,l,l,l,l,f},
+        {f,l,l,l,h,h,h,h,h,h,h,h,l,l,l,f},
+        {f,l,l,h,h,h,h,l,l,h,h,h,h,l,l,f},
+        {f,l,h,h,h,h,l,l,l,l,h,h,h,h,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,h,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f}};
+    static final int[][] B = {
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,l,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,h,l,l,f},
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f}};
+    static final int[][] C = {
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f},
+        {f,l,l,l,l,h,h,h,h,h,h,h,h,l,l,f},
+        {f,l,l,l,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,l,l,h,h,h,h,l,l,l,l,h,h,h,h,f},
+        {f,l,h,h,h,h,l,l,l,l,l,l,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,l,l,l,l,f},
+        {f,l,h,h,h,h,l,l,l,l,l,l,h,h,h,f},
+        {f,l,l,h,h,h,h,l,l,l,l,h,h,h,h,f},
+        {f,l,l,l,h,h,h,h,h,h,h,h,h,h,l,f},
+        {f,l,l,l,l,h,h,h,h,h,h,h,h,l,l,f},
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f}};
+    static final int[][] D = {
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f},
+        {f,h,h,h,h,h,h,h,h,h,h,l,l,l,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,l,l,l,f},
+        {f,h,h,h,h,l,l,l,l,h,h,h,h,l,l,f},
+        {f,h,h,h,h,l,l,l,l,l,h,h,h,h,l,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,l,h,h,h,h,f},
+        {f,h,h,h,h,l,l,l,l,l,h,h,h,h,l,f},
+        {f,h,h,h,h,l,l,l,l,h,h,h,h,l,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,h,l,l,l,f},
+        {f,h,h,h,h,h,h,h,h,h,h,l,l,l,l,f},
+        {f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f}};
+    
+    static final int[][] start = C;
+    static final int[][] end = D;
+    static int[][] change;
+    static int[][] middle;
+    static int[][] newMiddle;
 
     public static void main(String[] args) {
         setupMatrices();
@@ -42,53 +87,77 @@ public class MessingWithPixels {
 
         int step = 1;
         while (!matricesEqual(middle, end) && step < 10) {
+            /*for (int r = 1; r < middle.length - 1; r++) {
+                int c = 1;
+                if (change[r][c] == 1 && checkNeighborsEdges(middle, r, c)) {
+                    switcheroo(newMiddle, r, c);
+                }
+                c = middle.length - 2;
+                if (change[r][c] == 1 && checkNeighborsEdges(middle, r, c)) {
+                    switcheroo(newMiddle, r, c);
+                }
+            }
+            
+            for (int c = 1; c < middle[0].length - 1; c++) {
+                int r = 1;
+                if (change[r][c] == 1 && checkNeighborsEdges(middle, r, c)) {
+                    switcheroo(newMiddle, r, c);
+                }
+                r = middle.length - 2;
+                if (change[r][c] == 1 && checkNeighborsEdges(middle, r, c)) {
+                    switcheroo(newMiddle, r, c);
+                }
+            }*/
+            
             for (int i = 1; i < (middle.length - 1); i++) {
                 for (int j = 1; j < (middle[i].length - 1); j++) {
-                    if (change[i][j] && checkNeighbors(middle, i, j)) {
+                    if (change[i][j] == 1 && checkNeighborsInterior(middle, i, j)) {
                         switcheroo(newMiddle, i, j);
                     }
                 }
             }
             updateMiddle();
-            System.out.println("step " + step + ": "); printMatrix(middle);
+            System.out.println("\nstep " + step + ": "); printMatrix(middle);
             updateChange();
             //System.out.println("change array (step " + step + "): "); printMatrix(change);
             step++;
         }
     }
 
-    public static void switcheroo(boolean[][] a, int i, int j) {
-        if (a[i][j]) {
-            a[i][j] = false;
+    public static void switcheroo(int[][] a, int i, int j) {
+        if (a[i][j] == 2) {
+            a[i][j] = 0;
             return;
         }
-        if (!a[i][j]) {
-            a[i][j] = true;
+        if (a[i][j] == 0) {
+            a[i][j] = 2;
         }
     }
     
-    public static boolean matricesEqual(boolean[][] a, boolean[][] b) {
-        if (a.length != b.length) return false;
+    public static boolean matricesEqual(int[][] a, int[][] b) {
+        if (a.length != b.length) { System.out.println("rows not equal"); return false; }
         for (int i = 0; i < a.length; i++) {
-            if (a[i].length != b[i].length) return false;
+            if (a[i].length != b[i].length) { System.out.println("cols " + i + " not equal"); return false; }
             for (int j = 0; j < a[i].length; j++) {
-                if (a[i][j] != b[i][j]) return false;
+                if ((a[i][j] == 0 && b[i][j] == 2) || (a[i][j] == 2 && b[i][j] == 0)) { /*System.out.println(i + "," + j + " not equal");*/ return false; }
             }
         }
         return true;
     }
 
     public static void updateChange() {
-        change = new boolean[start.length][start[0].length];
+        change = new int[start.length][start[0].length];
         for (int i = 0; i < start.length; i++) {
             for (int j = 0; j < start[i].length; j++) {
-                change[i][j] = (!(middle[i][j] == end[i][j]));
+                //change[i][j] = (!(middle[i][j] == end[i][j]));
+                if (middle[i][j] == end[i][j]) change[i][j] = 0;
+                else change[i][j] = 1;
             }
         }
     }
 
     public static void updateMiddle() {
-        middle = new boolean[newMiddle.length][newMiddle[0].length];
+        middle = new int[newMiddle.length][newMiddle[0].length];
         for (int i = 0; i < newMiddle.length; i++) {
             for (int j = 0; j < newMiddle[i].length; j++) {
                 middle[i][j] = newMiddle[i][j];
@@ -96,31 +165,53 @@ public class MessingWithPixels {
         }
     }
 
-    public static boolean checkNeighbors(boolean[][] a, int r, int c) {
-        boolean d = a[r][c];
-        boolean corners = d != a[r - 1][c] || d != a[r + 1][c] || d != a[r][c - 1] || d != a[r][c + 1];
-        boolean diags = d != a[r - 1][c - 1] || d != a[r - 1][c + 1] || d != a[r + 1][c - 1] || d != a[r + 1][c + 1];
-        return diags || corners;
+    public static boolean checkNeighborsInterior(int[][] a, int row, int col) {
+        int val = a[row][col], up = a[row - 1][col], down = a[row + 1][col], left = a[row][col - 1], right = a[row][col + 1];
+        boolean cornerOne = (val == 0 && up == 2) || (val == 2 && up == 0);
+        boolean cornerTwo = (val == 0 && down == 2) || (val == 2 && down == 0);
+        boolean cornerThree = (val == 0 && left == 2) || (val == 2 && left == 0);
+        boolean cornerFour = (val == 0 && right == 2) || (val == 2 && right == 0);
+        boolean corners = cornerOne || cornerTwo || cornerThree || cornerFour;
+        
+        /*boolean diagOne = (d == 0 && a[r - 1][c] == H) || (d == 1 && a[r - 1][c] == H);
+        boolean diagTwo = (d == 0 && a[r + 1][c] == H) || (d == 1 && a[r + 1][c] == H);
+        boolean diagThree = (d == 0 && a[r][c - 1] == H) || (d == 1 && a[r][c - 1] == H);
+        boolean diagFour = (d == 0 && a[r][c + 1] == H) || (d == 1 && a[r][c + 1] == H);
+        boolean diags = diagOne || diagTwo || diagThree || diagFour;*/
+        
+        //return diags || corners;
+        return corners;
     }
     
-    public static void printMatrix(boolean[][] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                if (a[i][j]) System.out.print("M ");
-                else System.out.print("_ ");
+    /*public static boolean checkNeighborsEdges(int[][] a, int row, int col) {
+        int val = a[row][col], up = a[row - 1][col], down = a[row + 1][col], left = a[row][col - 1], right = a[row][col + 1];
+        boolean topEdge = up == 1 && (val != down || val == left || val == right);
+        boolean bottomEdge = down == 1 && (val == up || val == left || val == right);
+        boolean leftEdge = left == 1 && (val == up || val == down || val == right);
+        boolean rightEdge = right == 1 && (val == up || val == down || val == left);
+        return topEdge || bottomEdge || leftEdge || rightEdge;
+    }*/
+    
+    public static void printMatrix(int[][] a) {
+        for (int i = 1; i < a.length - 1; i++) {
+            for (int j = 1; j < a[i].length - 1; j++) {
+                if (a[i][j] == 2) System.out.print("M ");
+                else if (a[i][j] == 0) System.out.print("_ ");
+                else System.out.print("  ");
             }
             System.out.println();
         }
     }
     
     public static void setupMatrices() {
-        middle = new boolean[start.length][start[0].length];
+        
+        middle = new int[start.length][start[0].length];
         for (int i = 0; i < start.length; i++) {
             for (int j = 0; j < start[i].length; j++) {
                 middle[i][j] = start[i][j];
             }
         }
-        newMiddle = new boolean[middle.length][middle[0].length];
+        newMiddle = new int[middle.length][middle[0].length];
         for (int i = 0; i < middle.length; i++) {
             for (int j = 0; j < middle[i].length; j++) {
                 newMiddle[i][j] = middle[i][j];
